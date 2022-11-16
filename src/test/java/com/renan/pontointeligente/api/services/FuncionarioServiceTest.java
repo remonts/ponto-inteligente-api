7f975fb4adbed18605f037c8ebc369157333202d
Renan Monteiro <renan.monteiro@dtidigital.com.br>
2022-11-16 20:12:43 -0300

    test: create FuncionarioServiceTest.java

---
+++ src/test/java/com/renan/pontointeligente/api/services/FuncionarioServiceTest.java
@@ -0,0 +1,59 @@
+package com.renan.pontointeligente.api.services;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.renan.pontointeligente.api.entities.Funcionario;
+import com.renan.pontointeligente.api.repositories.FuncionarioRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class FuncionarioServiceTest {
+	@MockBean
+	private FuncionarioRepository funcionarioRepository;
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+
+	@Before
+	public void setUp() throws Exception {
+		BDDMockito.given(this.funcionarioRepository.save(Mockito.any(Funcionario.class))).willReturn(new Funcionario());
+		BDDMockito.given(this.funcionarioRepository.findByEmail(Mockito.anyString())).willReturn(new Funcionario());
+		BDDMockito.given(this.funcionarioRepository.findByCpf(Mockito.anyString())).willReturn(new Funcionario());
+	}
+
+	@Test
+	public void testPersistirFuncionario() {
+		Funcionario funcionario = this.funcionarioService.persistir(new Funcionario());
+
+		assertNotNull(funcionario);
+	}
+
+	@Test
+	public void testBuscarFuncionarioPorEmail() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorEmail("email@email.com");
+
+		assertTrue(funcionario.isPresent());
+	}
+
+	@Test
+	public void testBuscarFuncionarioPorCpf() {
+		Optional<Funcionario> funcionario = this.funcionarioService.buscarPorCpf("24291173474");
+
+		assertTrue(funcionario.isPresent());
+	}
+}
