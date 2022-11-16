fb37c792165863171fa20f6e7aa52f619bdb0d5b
Renan Monteiro <renan.monteiro@dtidigital.com.br>
2022-11-16 20:07:14 -0300

    feat: create FuncionarioServiceImpl.java

--- src/main/java/com/renan/pontointeligente/api/repositories/FuncionarioRepository.java
+++ src/main/java/com/renan/pontointeligente/api/repositories/FuncionarioRepository.java
@@ -1,5 +1,7 @@
 package com.renan.pontointeligente.api.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +9,7 @@ import com.renan.pontointeligente.api.entities.Funcionario;
 
 @Transactional(readOnly = true)
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+	Optional<Funcionario> findById(Long id);
 	
 	Funcionario findByCpf(String cpf);
 	
